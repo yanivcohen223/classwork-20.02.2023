d4c927e07b81775a4d6fa888008fa5e4073e9ab4
yanivcohen223 <yanivcohen223@gmail.com>
2023-02-21 17:28:33 +0200

    upload

--- homework2/src/main/java/org/example/Main.java
+++ homework2/src/main/java/org/example/Main.java
@@ -1,15 +1,27 @@
 package org.example;
 
 public class Main {
+    private static void startMusicShow(Player somePlayer) {
+        somePlayer.play();
+        if (somePlayer instanceof GuitarPlayer){
+            ((GuitarPlayer) somePlayer).rockAndRoll();
+        }
+        else {
+            ((PianoPlayer) somePlayer).playJazz();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        Player player1 = new GuitarPlayer("Jimi Hendrix", "PopRock");
+        Player player2 = new PianoPlayer("Elton John", "Pop");
 
 
-        IPlayer player1 = new GuitarPlayer("Jimi Hendrix", "PopRock");
-        IPlayer player2 = new PianoPlayer("Elton John", "Pop");
+        startMusicShow(player1);
+        System.out.println("");
+        startMusicShow(player2);
 
-        player1.play();
-        player2.play();
     }
+
+
 }
